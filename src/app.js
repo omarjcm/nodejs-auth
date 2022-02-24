259846e76802c8fda434433a38e8d08297a85ab6
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-02-24 17:29:43 -0500

    Implementacion del ruteo.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
 
+import productsRoutes from './routes/products.routes'
+
 const app = express()
 
 app.set('pkg', pkg)
@@ -16,5 +18,7 @@ app.get('/', (req, res) => {
         version: app.get('pkg').version,
     })
 })
+
+app.use('/products', productsRoutes)
 
 export default app;
