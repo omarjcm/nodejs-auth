f4e1fe07165713bb3f7ab431e1f4fa883dead309
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-02-24 17:05:42 -0500

    Acceder a los datos de package.json

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import morgan from 'morgan'
+import pkg from '../package.json'
 
 const app = express()
+
+app.set('pkg', pkg)
+
 app.use( morgan('dev') )
 
 app.get('/', (req, res) => {
     res.json({
-        author:'Guillermo Pizarro',
-        description:'',
-        version:'1.0.0'
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+        description: app.get('pkg').description,
+        version: app.get('pkg').version,
     })
 })
 
