0cd1c09c8d203eaa064cd7ccb6c15782c6678e3b
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-02-24 16:57:50 -0500

    Creacion de una respuesta mediante JSON

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
+app.use( morgan('dev') )
+
+app.get('/', (req, res) => {
+    res.json('welcome')
+})
 
 export default app;
